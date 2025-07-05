3622797a52f0c3c3288f39880683dbcfd3f922df
Leb <eniolaolagbegi@gmail.com>
2025-07-05 14:55:12 +0100

    :numeric string

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -1,13 +1,16 @@
 import { AppDatasource } from "./database"
+import { types as pgTypes } from 'pg';
 
 const connectToDb = async ()=>{
 try {
+pgTypes.setTypeParser(pgTypes.builtins.NUMERIC, (val: string) =>
+  val === null ? null : parseFloat(val)
+);
     await AppDatasource.initialize()
     console.info('Connected to database successfully')
 } catch (error) {
     console.error(error)
 }
-
 }
 const bootstrap = async ()=>{
     await connectToDb()
